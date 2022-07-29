3570708d3b24c471aa86927e1b995af9bc3216f0
austincondict-AngelOak <74200187+austincondict-AngelOak@users.noreply.github.com>
2022-07-29 17:24:44 -0400

    Fixed paths in pdf2json.js, updated test in example/node

--- example/node/pdf2json.js
+++ example/node/pdf2json.js
@@ -1,7 +1,7 @@
+/* OUTDATED -- NEEDS TO BE UPDATED TO USE PROMISE-BASED pdf2json.js IN '../../lib' */
+/*
 const root = __dirname + '/../../';
-
 var pdfParser = require(root + 'src/pdf2json/pdf2json.js');
-
 var PDF_PATH = root + 'files/test.pdf';
 
 pdfParser.pdf2json(PDF_PATH, function (error, pdf) {
@@ -11,3 +11,19 @@ pdfParser.pdf2json(PDF_PATH, function (error, pdf) {
         console.log(JSON.stringify(pdf));
     }
 });
+*/
+
+const path = require('path');
+const root = path.resolve(__dirname, '..', '..');
+const pdfParser = require(path.join(root, 'lib', 'pdf2json.js'));
+const PDF_PATH = path.join(root, 'files', 'test.pdf');
+
+(async () => {
+    try {
+        const pdf = await pdfParser.pdf2json(PDF_PATH);
+        console.dir(pdf);
+    }
+    catch (err) {
+        console.error(err.stack);
+    }
+})();
